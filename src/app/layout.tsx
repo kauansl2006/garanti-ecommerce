d191d4bf5aea1ef63ee00572cb612b318a78a779
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-06-16 11:07:44 -0300

    style: formatar arquivos com eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,27 @@ import { Metadata } from "next";
 import "./globals.css";
 
 const montserrat = Montserrat({
-  weight: ["300", "400", "500", "600", "700"],
-  subsets: ["latin"],
-  variable: "--font-montserrat"
+    weight: ["300", "400", "500", "600", "700"],
+    subsets: ["latin"],
+    variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
-  title: "Garanti",
-  description: "Template de e-commerce ideal para negócios que atuam no Brasil."
-}
+    title: "Garanti",
+    description:
+        "Template de e-commerce ideal para negócios que atuam no Brasil.",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt">
-      <body className={`${montserrat.className} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pt">
+            <body className={`${montserrat.className} antialiased`}>
+                {children}
+            </body>
+        </html>
+    );
 }
