04a4d48424b91f3870b514fc53bae586cffe9d00
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-06-16 19:36:00 -0300

    feat: criar componente footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function Home() {
     return (
         <>
             <Header />
-            <main>
+            <main className="w-screen h-[60vh] flex flex-col item-center justify-center px-[300px]">
                 <p>HomePage Works!</p>
             </main>
+            <Footer />
         </>
     );
 }
