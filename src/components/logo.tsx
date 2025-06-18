327257322776318aa971cfe51fd2e7a23f30f04b
Kauan Santos Lima <kauansantosl2006@gmail.com>
2025-06-18 16:20:59 -0300

    refactor: atualizar props do componente logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,13 +2,24 @@
 
 import Image from "next/image";
 
-export default function Logo({ src } : { src: string}) {
+export default function Logo({ 
+    src,
+    width,
+    height,
+    className,
+} : { 
+    src: string;
+    width: number;
+    height: number;
+    className?: string | undefined;
+}) {
     return (
         <Image 
             src={src}
-            width={200}
-            height={70}
+            width={width}
+            height={height}
             alt="Logo da Garanti"
+            className={className}
         />
     )
 }
